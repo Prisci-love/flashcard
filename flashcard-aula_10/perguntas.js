323999f17d684299a5bf96e6fd43f937c10ef9f5
Priscii <161730431+Prisci-love@users.noreply.github.com>
2024-10-07 08:15:40 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'A.T',
+    'Quem foi Moisés?',
+    'Foi um profeta que libertou o povo de israel do egito para a terra prometida e escreveu os mandamentos'
+)
+
+criaCartao(
+    'A.T',
+    'Quantos mandamentos existem?',
+    '613, mas foram resumidos a 10'
+)
+
+criaCartao(
+    'N.T',
+    'Qual o nome dos apóstolos',
+    'Simão(Pedro), André (irmão de Pedro), Tiago(filho de Zebedeu), João(irmão de Tiago), Filipe, Bartolomeu, Tomé, Mateus, Simão Zelote e Judas Iscariotes, que mais tarde traiu Jesus'
+)
+
+criaCartao(
+    'N.T',
+    'Quem substituiu Judas?',
+    'Matias'
+)
+
+criaCartao(
+    'N.T',
+    'Porque Jesus desceu na terra?',
+    'Para nos salvar da consequência do pecado, que é a morte, o sangue derramado na cruz nos purificou e transformou nossa natureza do pecado, para virarmos justos e filhos de Deus'
+)
+
+criaCartao(
+    'N.T',
+    'O que é a graça? estamos vivendo na graça ou na lei?',
+    'graça é favor imerecido, e estamos vivendo nela'
+)
